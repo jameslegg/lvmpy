1bf7beeb95df272892e79a7207c214d509193290
James Legg <james@buntyssd>
2012-09-02 13:13:48 +0100

    Added tests to verify functionality of snapshot()

--- test.py
+++ test.py
@@ -10,11 +10,12 @@ class TestLvFunctions(unittest.TestCase):
   vg_name = "testvg"
   test_snap = 'testsnap1'
   #supplied in the format LVM2 stores sizes to make validation easier
-  test_snap_size = '3.00g'
+  test_snap_size = '2.00g'
   lv_size = "4.00g"
   def setUp(self):
     self.testlv = lvm.lv()
     self.testlv.create(self.lv_name, self.vg_name, self.lv_size)
+    self.testlvsnapshot = self.testlv.snapshot(self.test_snap, self.test_snap_size)
 
   def test_size(self):
     self.assertEqual(self.lv_size, self.testlv.attr('LVM2_LV_SIZE'))
@@ -26,14 +27,20 @@ class TestLvFunctions(unittest.TestCase):
     self.assertEqual(self.vg_name, self.testlv.attr('LVM2_VG_NAME'))
 
   def test_create_snapshot(self):
-    new_snap = self.testlv.snapshot(self.test_snap, self.test_snap_size)
-    self.assertEqual(new_snap.attr('LVM2_LV_NAME'), self.test_snap)
-    new_snap.remove()
+    new_snap = self.testlv.snapshot('cows', '300m')
     
+  def test_remove_snapsnot(self):
+    self.testlvsnapshot.remove()
+
+  def test_name_snap(self):
+    self.assertEqual(self.test_snap, self.testlvsnapshot.attr('LVM2_LV_NAME'))
+
+  def test_size_snap(self):
+    self.assertEqual(self.test_snap_size, self.testlvsnapshot.attr('LVM2_LV_SIZE'))
 
   def tearDown(self):
     #can't delete an LV immediatly after creation
-    time.sleep(0.5)
+    time.sleep(0.1)
     self.testlv.remove()
 
 if __name__ == '__main__':
